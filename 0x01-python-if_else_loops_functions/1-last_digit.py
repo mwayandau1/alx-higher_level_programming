a250fab880af632e8bb738a1bbeb2db47d2c3853
mwayandau1 <mosesayandau1@gmail.com>
2022-10-05 10:03:51 +0000

    Second commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ if n < 0:
 n = n % 10
 
 if n == 0:
-    print("Last digit of ", number, "is", n, "and is zero")
+    print(f"Last digit of  {number} is {n} and is 0")
 elif n > 5:
-    print("Last digit of", number, "is", n, "and is greater than 5")
+    print(f"Last digit of {number} is {n} and is greater than 5")
 elif n < 6 and n != 0:
-    print("Last digit of", number, "is", n, "and is less than 6 and not 0")
+    print(f"Last digit of {number} is {n} and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print(str.upper())
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print("")
