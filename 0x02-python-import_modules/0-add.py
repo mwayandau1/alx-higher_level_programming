588c7b57bedae2542dfa0d4e008899929b0bce07
mwayandau1 <mosesayandau1@gmail.com>
2022-10-05 10:47:53 +0000

    First commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,1 +1,8 @@
-from add_0 import add
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print the sum of two numbers """
+    from add_0 import add
+    a=1
+    b=2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
